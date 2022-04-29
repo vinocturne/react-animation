70343ffc910d4e03250c74f04af7a3faade1bed7
Ghunhee Shin <hago@rocketstaff.com>
2022-04-29 16:14:05 +0900

    Feat(variants) : "추가 Variants"
    variants 기능을 활용해 태그와 애니메이션 속성들을 분리하였습니다.

--- src/App.tsx
+++ src/App.tsx
@@ -8,25 +8,64 @@ const Wrapper = styled.div`
     justify-content: center;
     align-items: center;
 `;
-//styled-components에서 motion을 사용하기 위해서는 styled(motion.태그명)을 사용해야한다.
+
 const Box = styled(motion.div)`
     width: 200px;
     height: 200px;
-    background-color: white;
-    border-radius: 15px;
+    display: grid;
+    grid-template-columns: repeat(2, 1fr);
+    background-color: rgba(255, 255, 255, 0.2);
+    border-radius: 40px;
     box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
 `;
+
+const Circle = styled(motion.div)`
+    background-color: white;
+    height: 70px;
+    place-self: center;
+    width: 70px;
+    border-radius: 35px;
+    box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
+`;
+
+const boxVariants = {
+    start: {
+        opacity: 0,
+        scale: 0.5,
+    },
+    end: {
+        scale: 1,
+        opacity: 1,
+        transition: {
+            type: "spring",
+            duration: 0.5,
+            bounce: 0.5,
+            delayChildren: 0.5,
+            staggerChildren: 0.2,
+        },
+    },
+};
+
+const circleVariants = {
+    start: {
+        opacity: 0,
+        y: 10,
+    },
+    end: {
+        opacity: 1,
+        y: 0,
+    },
+};
 
 function App() {
     return (
         <Wrapper>
-            <Box
-                transition={{ type: "spring", delay: 0.5 }}
-                initial={{ scale: 0 }}
-                animate={{ scale: 1, rotateZ: 360 }}
-            />
-            {/* //motion을 사용하기위해 div를 선언하려면 motion.div를 사용해야한다. */}
-            <motion.div></motion.div>
+            <Box variants={boxVariants} initial="start" animate="end">
+                <Circle variants={circleVariants} />
+                <Circle variants={circleVariants} />
+                <Circle variants={circleVariants} />
+                <Circle variants={circleVariants} />
+            </Box>
         </Wrapper>
     );
 }
